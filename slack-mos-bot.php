3b827ecae12df979b7992d8dc03ca1afe5da90ad
Kyle Benson <kyle@psadmin.io>
2016-12-09 12:22:56 -0600

    default type and pum date updates
    added default type to `doc` if missing, and updated pum dates

--- slack-mos-bot.php
+++ slack-mos-bot.php
@@ -11,84 +11,94 @@
         $text = $_POST['text'];
         $token = $_POST['token'];
 
-        // validate token
-        if($token != $token_key){
-           $msg = "The token for the slack command doesn't match. Check your script.";
-           die($msg);
-           echo $msg;
-        }
+    	// validate token
+	if($token != $token_key){ 
+ 	   $msg = "The token for the slash command doesn't match. Check your script.";
+    	   die($msg);
+       	   echo $msg;
+	}
 
-        // process request
-        $text_array = explode(" ",$text);
-        $type = $text_array[0];
-        $param = $text_array[1];
+	// process request
+	$text_array = explode(" ",$text);
+	$type = $text_array[0];
+	$param = $text_array[1];
+	
+	/* Take any text passed 2nd param and make it a string
+	unset($text_array[0]);
+	unset($text_array[1]);
+	$descr = implode(" ",$text_array);
+	*/
+	
+	switch ($type) {
+		case doc:
+			$reply = 'https://support.oracle.com/epmos/faces/DocumentDisplay?id=' . $param;
+			break;
+		case bug:
+			$reply = 'https://support.oracle.com/epmos/faces/BugMatrix?id=' . $param;
+			break;
+		case patch:
+			$reply = 'https://support.oracle.com/epmos/faces/PatchResultsNDetails?patchId=' . $param;
+			break;
+		case pum:
+			$reply = generate_pum($param);
+			break;
+		case dpk:
+			$reply = 'https://support.oracle.com/epmos/faces/DocumentDisplay?id=2127715.2';
+			break;
+		case idea:
+			$reply = 'https://community.oracle.com/ideas/' . $param;
+			break;
+		default:
+			if (preg_match('/^\d/', $type) === 1)
+				// if number, default doc type
+				$reply = 'https://support.oracle.com/epmos/faces/DocumentDisplay?id=' . $type;
+			else
+				$reply = generate_help();
+			break;
+	}
 
-        switch ($type) {
-                case doc:
-                        $reply = 'https://support.oracle.com/epmos/faces/DocumentDisplay?id=' . $param;
-                        break;
-                case bug:
-                        $reply = 'https://support.oracle.com/epmos/faces/BugMatrix?id=' . $param;
-                        break;
-                case patch:
-                        $reply = 'https://support.oracle.com/epmos/faces/PatchResultsNDetails?patchId=' . $param;
-                        break;
-                case pum:
-                        $reply = generate_pum($param);
-                        break;
-                case dpk:
-                        $reply = 'https://support.oracle.com/epmos/faces/DocumentDisplay?id=2127715.2';
-                        break;
-                case idea:
-                        $reply = 'https://community.oracle.com/ideas/' . $param;
-                        break;
-                default:
-                        $reply = generate_help();
-        }
+	// process response
+	$response = array();
+	$response["response_type"] = "in_channel";
+	$response["text"] = $reply;
 
-        // process response
-        $response = array();
-        $response["response_type"] = "in_channel";
-        $response["text"] = $reply;
+	header('Content-Type: application/json');
+	echo json_encode($response);
 
-        header('Content-Type: application/json');
-        echo json_encode($response);
+	function generate_help() {
+		return "Slackbot Help - https://psadminio.slack.com/files/kyle/F12PYDA3S/Slackbot_Help";
+	}
+	
+	function generate_pum($param) {
+		switch ($param) {
+			case hcm:
+			case hr:
+				$result = '`Image 020`	December 9, 2016 `Image 021` January 20, 2017 `Image 022` April 21, 2017 `Image 023`	July 14, 2017 `Image 024` October 6, 2017'; 
+				break;
+			case fscm:
+			case fin:
+				$result = '`Image 022`	December 16, 2016 `Image 023` March 13, 2017 `Image 024` June 12, 2017 `Image 025` September 11, 2017 `Image 026` December 11, 2017'; 
+				break;
+			case elm:
+				$result = '`Image 015`	January 27, 2017 `Image 016` July 28, 2017'; 
+				break;
+			case ih:
+			case portal:
+				$result = '`Image 003`	February 15, 2017 `Image 004` July 14, 2017 `Image 005`	December 1, 2017'; 
+				break;
+			case cs:
+				$result = '`Image 004`	February 7, 2017 `Image 005` May 2, 2017 `Image 006` July 25, 2017 `Image 007` October 31, 2017'; 
+				break;
+			case crm:
+				$result = '`Image 013` February 3, 2017 `Image 014` July 28, 2017'; 
+				break;
+			case home:
+				$result = 'PUM Home Page - https://support.oracle.com/epmos/faces/DocumentDisplay?id=1641843.2'; 
+				break;
+			default:				
+				$result = generate_help();				
+		}
 
-        function generate_help() {
-                return "Slackbot Help - $helpfile";
-        }
-
-        function generate_pum($param) {
-                switch ($param) {
-                        case hcm:
-                        case hr:
-                                $result = '`Image 19` October 7, 2016 `Image 20` December 9, 2016';
-                                break;
-                        case fscm:
-                        case fin:
-                                $result = '`Image 19` June 27, 2016 `Image 20` August 22, 2016 `Image 21` October 17, 2016 `Image 22` December 12, 2016';
-                                break;
-                        case elm:
-                                $result = '`Image 14` September 23, 2016';
-                                break;
-                        case ih:
-                        case portal:
-                                $result = '`Image 3` August 4, 2016 `Image 4` December 15, 2016';
-                                break;
- case cs:
-                                $result = '`Image 2` July 26, 2016 `Image 3` November 1, 2016';
-                                break;
-                        case crm:
-                                $result = '`Image 12` September 23, 2016';
-                                break;
-                        case home:
-                                $result = 'PUM Home Page - https://support.oracle.com/epmos/faces/DocumentDisplay?id=1641843.2';
-                                break;
-                        default:
-                                $result = generate_help();
-                }
-
-                return $result;
-        }
+		return $result;
+	}	
 ?>
-                           
